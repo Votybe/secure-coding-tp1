c3af8ad88b35d5b1f18ffdc22c4bd2e69efdda36
Votybe <aymmai@gmail.com>
2022-11-16 17:52:03 +0100

    enlever env de git

--- src/lib/typeorm.ts
+++ src/lib/typeorm.ts
@@ -13,5 +13,5 @@ export const AppDataSource = new DataSource({
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  entities: ["entity/*.ts"],
+  entities: ["entities/*.ts"],
 });
