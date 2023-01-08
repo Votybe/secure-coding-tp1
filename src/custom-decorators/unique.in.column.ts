dd954873ccfc0a156550a5b0ff25cd0f86b9dfe5
Karea <thomas.meedendorp@efrei.net>
2023-01-08 14:47:12 +0100

    Exercice 6 done but doesn't work as User metadata can't be found

--- src/custom-decorators/unique.in.column.ts
+++ src/custom-decorators/unique.in.column.ts
@@ -1,21 +1,25 @@
-import {
-    registerDecorator,
-    ValidationOptions,
-    ValidatorConstraint,
-    ValidatorConstraintInterface,
-  } from "class-validator";
-import { User } from "../entities/users";
+import { registerDecorator, ValidationOptions, ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
 import { AppDataSource } from "../lib/typeorm";
+import { User } from "../entities/users";
 
-@ValidatorConstraint()
-export class UniqueInColumn implements ValidatorConstraintInterface {
-  validate(emailValue: string) {
-    console.log("ezdzedzefdzfzpjfzfkzef", emailValue)
-    console.log("1", AppDataSource.getRepository(User));
-    return AppDataSource.getRepository(User).findOne({where: {email: emailValue,}}).then((user: any) => {
-      if (user)
-        return false;
+@ValidatorConstraint({ async: true })
+export class UniqueInColumnConstraint implements ValidatorConstraintInterface {
+  validate(value: any, args: ValidationArguments) {
+    return AppDataSource.getRepository(User).findOne({ where: { email: value} }).then(user => {
+      if (user) return false;
       return true;
     });
   }
+}
+
+export function UniqueInColumn(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: UniqueInColumnConstraint,
+    });
+  };
 }
